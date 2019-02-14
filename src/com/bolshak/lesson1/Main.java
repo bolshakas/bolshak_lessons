78399722a879d6528cacfbe896ebe12a0a4e96cf
Bolshak Anna <ignatkoas@gmail.com>
2019-02-15 01:10:46 +0200

    add .gitignore file and first lesson

---
+++ src/com/bolshak/lesson1/Main.java
@@ -0,0 +1,22 @@
+package com.bolshak.lesson1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Enter name -> ");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello "+ name);
+
+        System.out.println(" *** ");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("*   *");
+        System.out.println(" *** ");
+    }
+}
