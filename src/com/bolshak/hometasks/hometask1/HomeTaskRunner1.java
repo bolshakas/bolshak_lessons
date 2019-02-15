52b5a33a4e59d314b81734088f727dd787e0bbde
Bolshak Anna <ignatkoas@gmail.com>
2019-02-15 22:05:50 +0200

    Add home task1

---
+++ src/com/bolshak/hometasks/hometask1/HomeTaskRunner1.java
@@ -0,0 +1,16 @@
+package com.bolshak.hometasks.hometask1;
+
+public class HomeTaskRunner1 {
+
+    public static void main(String[] args) {
+        printEpam();
+    }
+
+    private static void printEpam () {
+        System.out.println("****  ****    *     *     *");
+        System.out.println("*     *  *   * *    * * * *");
+        System.out.println("**    ****  *   *   *  *  *");
+        System.out.println("*     *    * *** *  *     *");
+        System.out.println("****  *   *       * *     *");
+    }
+}
